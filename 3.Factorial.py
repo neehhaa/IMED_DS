45318ef74a3d62d779008824528852089b8d68e4
neehhaa <nehawasnik95@outlook.com>
2022-04-22 23:36:16 +0530

    Factorial

---
+++ 3.Factorial.py
@@ -0,0 +1,7 @@
+# Write a program to display the factorial of a given number.
+
+num = int(input("Enter the number : "))
+factorial = 1
+for i in range(1, num+1):
+    factorial = factorial*i
+print("Factorial of ", num, "is ", factorial)
