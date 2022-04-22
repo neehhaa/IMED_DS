33cd16cdda613f215aede9c987d2ab10e105188c
neehhaa <nehawasnik95@outlook.com>
2022-04-22 20:44:31 +0530

    Addition of two numbers

---
+++ 2.Addition.py
@@ -0,0 +1,6 @@
+# Write a program to display the addition of two numbers.
+
+a = int(input("Enter First number : "))
+b = int(input("Enter Second number : "))
+c=a+b
+print("Addition of Two mumbers are", c)
