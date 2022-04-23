21cc8684e6467501ccab76257358f642e3ceb2e3
neehhaa <nehawasnik95@outlook.com>
2022-04-23 16:18:20 +0530

    Even and Odd List

---
+++ Even_Odd.py
@@ -0,0 +1,14 @@
+# To print even numbers and odd numbers from 1 to 10.
+
+even_list = []
+odd_list = []
+for i in range(0, 11):
+    if i % 2 == 0:
+        even_list.append(i)
+    else:
+        odd_list.append(i)
+print(even_list)
+print(odd_list)
+
+
+
