d28d1ff732840b0c435d9921110b7327699944e0
neehhaa <nehawasnik95@outlook.com>
2022-05-07 09:45:25 +0530

    Star pattern

---
+++ 7.Star_Pattern.py
@@ -0,0 +1,14 @@
+"""
+Pattern Printing
+*
+* *
+* * *
+* * * *
+"""
+
+rows = int(input("Enter the number of rows : "))
+for i in range(0,rows):
+    for j in range(0,i+1):
+        print("* ",end=" ")
+    print()
+
