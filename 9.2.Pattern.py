027741c830001fe0d2aae108bbbd87ab16cf8f2e
neehhaa <nehawasnik95@outlook.com>
2022-06-22 18:48:45 +0530

    pattern2

--- 9.2.Pattern.py
+++ 9.2.Pattern.py
@@ -9,3 +9,12 @@
 * * * * *
 
 """
+
+rows = 5#int(input("Enter the number of rows : "))
+rows = rows*2
+for i in range(1, rows+1):
+    if i % 2 == 1:
+        for j in range(1, i+1, 2):
+
+            print("*", end=" ")
+    print()
